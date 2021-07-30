1478c302a14538b5483e74b8e39e83bd3820563a
rider7 <brandon.rider@gmail.com>
2021-07-29 22:37:07 -0500

    Successful db connection!

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.*;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import sample.Utilities.DBConnection;
 
 public class Main extends Application {
 
@@ -18,6 +19,9 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+
+        //Establish connection before launch
+        DBConnection.startConnection();
         launch(args);
     }
 
--- src/sample/Utilities/DBConnection.java
+++ src/sample/Utilities/DBConnection.java
@@ -1,5 +1,41 @@
 package sample.Utilities;
 
+import javax.sql.*;
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
 
+    //JDBC URL parts
+    private static final String protocol = "jdbc";
+    private static final String vendorName= ":mysql:";
+    private static final String ipAddress ="//wgudb.ucertify.com/WJ062s3";
+
+    //JDBC URL concatenated
+    private static final String jdbcURL = protocol + vendorName + ipAddress;
+
+    //Driver interface reference
+    //private static final String mySQLJDBCDriver = "com.mysql.jdbc.Driver";
+    //private static final String mySQLJDBCDriver = "com.sql.Driver";
+    private static final String mySQLJDBCDriver = "com.mysql.cj.jdbc.Driver";
+
+    private static Connection conn = null;
+
+    private static final String userName = "U062s3"; //Username
+
+    private static final String password = "53688675267"; //Password
+
+    public static Connection startConnection(){
+        System.out.println("Trying to connect!");
+        try {
+            Class.forName(mySQLJDBCDriver);
+            conn = (Connection)DriverManager.getConnection(jdbcURL, userName, password);
+            System.out.println("Connection is Successful!!!!!!");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
 }
